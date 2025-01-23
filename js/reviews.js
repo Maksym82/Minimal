09fb8cf0e74281044baf1175685af19c11bdb352
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-01-23 15:13:10 +0200

    Update styles reviews

--- js/reviews.js
+++ js/reviews.js
@@ -17,17 +17,17 @@ const swiper = new Swiper('.slider-clients-reviews', {
     // when window width is >= 530px
     590: {
       slidesPerView: 2,
-      spaceBetween: 10,
+      spaceBetween: 20,
     },
     // when window width is >= 768px
     768: {
-      slidesPerView: 3,
-      spaceBetween: 20,
+      slidesPerView: 2,
+      spaceBetween: 50,
     },
     // when window width is >= 1280px
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 10,
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 120,
     },
   },
 
